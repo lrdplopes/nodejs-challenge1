c811895838abcce3ef14ff6d74064a4438cc63eb
lrdplopes <lrdp.lopes@gmail.com>
2021-01-05 10:53:19 -0300

    code completed

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
@@ -10,24 +10,80 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get("/repositories", (req, res) => {
+  return res.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post("/repositories", (req, res) => {
+  const { title, url, techs } = req.body;
+
+  const project = {
+    id: uuidv4(),
+    title: "Node.js - Challenge 1",
+    url: "https://github.com/lrdplopes/nodejs-challenge1",
+    techs: "Node.js",
+    likes: 0,
+  };
+
+  repositories.push(project);
+  return res.json(project);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", (req, res) => {
+  const { id } = req.params;
+  const { title, url, techs } = req.body;
+
+  const projectFindIndex = repositories.findIndex((project) => {
+    project.id === id;
+  });
+
+  if (!projectFindIndex === -1) {
+    return res.status(400).json({ error: "Project doesn't exists" });
+  }
+
+  const projectsRepository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[projectFindIndex].likes,
+  };
+
+  repositories[projectFindIndex] = projectsRepository;
+
+  return res.json(projectsRepository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", (req, res) => {
+  const { id } = req.params;
+
+  const projectFindIndex = repositories.findIndex(
+    (project) => project.id === id
+  );
+
+  if (projectFindIndex >= 0) {
+    repositories.splice(projectFindIndex, 1);
+  } else {
+    return res.status(400).json({ error: "Project doesn't exists" });
+  }
+
+  return res.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", (req, res) => {
+  const { id } = req.params;
+
+  const projectFindIndex = repositories.findIndex(
+    (project) => project.id === id
+  );
+
+  if (projectFindIndex === -1) {
+    return res.status(400).json({ error: "Project doesn't exists" });
+  }
+
+  repositories[projectFindIndex].likes++;
+
+  return res.json(repositories[projectFindIndex]);
 });
 
 module.exports = app;
